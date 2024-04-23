8308d53626c8432327bad792ba59370374d8580d
Zwandiboss08 <stefanussuwandi08@gmail.com>
2024-04-23 10:48:29 +0700

    add features login and logout create Factories Database to populate users

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -245,10 +245,10 @@
                 <img alt="image"
                     src="{{ asset('img/avatar/avatar-1.png') }}"
                     class="rounded-circle mr-1">
-                <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div>
+                <div class="d-sm-none d-lg-inline-block">Hi, Stefanus Suwandi</div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
-                <div class="dropdown-title">Logged in 5 min ago</div>
+                {{-- <div class="dropdown-title">Logged in 5 min ago</div>
                 <a href="features-profile.html"
                     class="dropdown-item has-icon">
                     <i class="far fa-user"></i> Profile
@@ -261,11 +261,14 @@
                     class="dropdown-item has-icon">
                     <i class="fas fa-cog"></i> Settings
                 </a>
-                <div class="dropdown-divider"></div>
-                <a href="#"
-                    class="dropdown-item has-icon text-danger">
+                <div class="dropdown-divider"></div> --}}
+                <a href="#" class="dropdown-item has-icon text-danger"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit()">
                     <i class="fas fa-sign-out-alt"></i> Logout
                 </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
             </div>
         </li>
     </ul>
